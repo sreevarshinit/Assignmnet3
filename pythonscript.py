5232e650bc619177b9ae7d856fd0686eb2abfe3a
sreevarshinit <thammineni.s@northeastern.edu>
2021-06-24 23:23:32 -0700

    added a line of code to display time date day

--- pythonscript.py
+++ pythonscript.py
@@ -1,6 +1,8 @@
-#script to dispay time
+#script to dispay time day date
 
 import datetime
 
 display = datetime.datetime.now()
 print (display.strftime("%I:%M:%S %p"))
+print (display.strftime("%d/%m/%Y"))
+print (display.strftime("%a"))
